e685baa400757d726e5d2e6d989f251aef266a8f
cweinberger <christian.weinberger@me.com>
2018-06-15 14:10:17 +0200

    Update bottle root url

--- mysql.rb
+++ mysql.rb
@@ -5,7 +5,7 @@ class Mysql < Formula
   sha256 "5b2a61700af7c99f5630a7dfdb099af9283c3029843cddd9e123bcdbcc4aad03"
 
   bottle do
-    root_url "https://homebrew.bintray.com/bottles-tap/"
+    root_url "https://homebrew.bintray.com/bottles/"
     sha256 "55b0311fdd9b9d6e369f3b7ea0c7c82692073502215ac7a00055a2189d5478a2" => :high_sierra
     sha256 "8e01836f50bf3f907e2357384442e8c938615c5fe2ba35f9e255aa08ff6bca0d" => :sierra
     sha256 "1a190fa7803e5706c690b8281f61e861a5db68781ee2193c17119d4dd1d0cb1d" => :el_capitan
